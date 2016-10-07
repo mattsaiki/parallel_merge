bf9d45180c0bc6cd68d0509e05bf84bddac66d60
Alex Alvarez <alyxtime@gmail.com>
2016-10-06 19:51:44 -0700

    checking

--- mergesort.c
+++ mergesort.c
@@ -5,7 +5,27 @@ Class: USD Comp 494
 Date: October 2016
 */
 
+// being includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <math.h>
+#include "mergesortparallel.h"
+// end includes
+
+//Begin forward declare
+//End forward declare
+
+
+
 
 int main ( int argc, char* argv[]){
+	// enter in command line arguments in form for thread_count and array_size
+
+	int thread_count = atoi(argv[1]);
+	long array_size = atol(argv[2]);
 
 }
