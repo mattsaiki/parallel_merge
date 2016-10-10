2582088b3b729a33d652525e692f89dd1e491eb1
Alex Alvarez <alyxtime@gmail.com>
2016-10-09 17:09:55 -0700

    serialtime is computing correctly

--- mergesort.c
+++ mergesort.c
@@ -32,56 +32,62 @@ int* parallel_array;
 int* parallel_array_temp;
 int thread_count;
 long array_size;
-//pthread_barrier_t my_barrier;
 // end global variable
 
 //Begin Main
 int main ( int argc, char* argv[]){
-	
+	if(argc != 3){ printf(" Wrong number of arguments\n"); exit(0);}	
 	// enter in command line arguments in form for thread_count and array_size
 	thread_count = atoi(argv[1]);
 	array_size = atol(argv[2]);
+	if(thread_count < 0 || array_size < 0){ printf(" please enter valid argument size postive values only\n"); exit(0);}
 	// end command line arguments
-
-	
-	//pthread_barrier_init(&my_barrier,NULL,thread_count);
 	
 	time_t time_of_day;
 	time_of_day = time(NULL);
 	srand((unsigned int)(time_of_day));
-	
-	// mallocing for array
+	struct timeval tv1,tv2;	
+	// mallocing for array with error checking
 	serial_array = malloc(sizeof(int)*array_size);
+	
 	if(serial_array == NULL){
-	printf(" malloc failed, program exit\n");
-	exit(1);
+		printf(" malloc failed, program exit\n");
+		exit(1);
 	}
+
 	serial_array_temp = malloc(sizeof(int)*array_size);
+	
 	if(serial_array_temp == NULL){
-	printf(" malloc failed, program exit\n");
-	exit(1);
+		printf(" malloc failed, program exit\n");
+		exit(1);
 	}
+
 	parallel_array = malloc(sizeof(int)*array_size);
+	
 	if(parallel_array == NULL){
-	printf(" malloc failed, program exit\n");
-	exit(1);
+		printf(" malloc failed, program exit\n");
+		exit(1);
 	}
+
 	parallel_array_temp = malloc(sizeof(int)*array_size);
+	
 	if(parallel_array_temp == NULL){
-	printf(" malloc failed, program exit\n");
-	exit(1);
+		printf(" malloc failed, program exit\n");
+		exit(1);
 	}
 	// end malloc
-
+	// fill array with randomvalues
 	generateRandomValues();
 	// performing serial sort
+	gettimeofday(&tv1,NULL);	
 	serialSort(0,array_size-1);
-	for(int i = 0; i < array_size; i++){
-		printf(" serial array: %d\n",serial_array[i]);
-		printf(" serial_temp_array: %d\n",serial_array_temp[i]);
-	}
+	gettimeofday(&tv2,NULL);
+	
 	int check = validateSort(array_size);
 	if(check != 0){printf("sorting failed\n");}
+	double serial_time = (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 + (double) (tv2.tv_sec - tv1.tv_sec);	
+	printf("SerialTime is:%f\n",serial_time);
+	// freeing memory
 	free(serial_array);
 	free(serial_array_temp);
 	free(parallel_array);
@@ -165,12 +171,9 @@ void serialMergeSort(int low, int mid, int high){
 int validateSort(int numElements){
         int i, j;
         for(i=0; i<numElements; i++){
-		if(serial_array[i] != serial_array_temp[i]){
-		return 1;}
-                for(j=i; j<numElements; j++){
-                        if(serial_array[i]>serial_array[j]){
-                                return 1;
-                        }
+		if(serial_array[i] != serial_array_temp[i]){return 1;}
+		for(j=i; j<numElements; j++){
+			if(serial_array[i]>serial_array[j]){ return 1;}
                 }
         }
         return 0;
